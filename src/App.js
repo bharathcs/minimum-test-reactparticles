3cd04d1216c863a24a41f25569cc6a7a13f08ec2
Bharath Chandra Sudheer <bharath.sudheer@gmail.com>
2021-06-29 11:15:00 +0800

    Install react-particles and use.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import Particles from "react-particles-js";
+import Particles from "react-particles-js";
 
 function App() {
-  // return <Particles />;
-  return <div>boo</div>;
+  return <Particles />;
+  // return <div>boo</div>;
 }
 
 export default App;
